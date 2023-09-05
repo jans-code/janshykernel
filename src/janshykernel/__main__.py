f661421dca2ca46d057b0f7ddb39466ea60b41a4
jans-code <7jantb@gmx.de>
2023-09-05 15:44:52 +0200

    refactored shebang

--- src/janshykernel/__main__.py
+++ src/janshykernel/__main__.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+# *_* coding: utf-8 *_*
+
+""" Main module """
+
 from ipykernel.kernelapp import IPKernelApp
 from .kernel import janshykernel
 IPKernelApp.launch_instance(kernel_class=janshykernel)
